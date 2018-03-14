a026e417987b2a20c69aeff1c494b85e1c223695
鈴木 白斗 <hakutosuzuki@crooz.co.jp>
2018-03-14 13:08:00 +0900

    add bottomappbar

--- app/src/main/java/com/example/hakutosuzuki/p_alpha_slibrarysampler/BottomAppBarActivity.kt
+++ app/src/main/java/com/example/hakutosuzuki/p_alpha_slibrarysampler/BottomAppBarActivity.kt
@@ -2,11 +2,30 @@ package com.example.hakutosuzuki.p_alpha_slibrarysampler
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.bottomappbar.BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+import android.support.design.bottomappbar.BottomAppBar.FAB_ALIGNMENT_MODE_END
+import android.support.design.widget.FloatingActionButton.SIZE_MINI
+import android.support.design.widget.FloatingActionButton.SIZE_NORMAL
+import kotlinx.android.synthetic.main.activity_bottom_app_bar.*
 
 class BottomAppBarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_app_bar)
+
+
+        button_center.setOnClickListener {
+            bottomappbar.fabAlignmentMode = FAB_ALIGNMENT_MODE_CENTER
+        }
+
+        button_end.setOnClickListener {
+            bottomappbar.fabAlignmentMode = FAB_ALIGNMENT_MODE_END
+        }
+
+        button_fab.setOnClickListener {
+            if (fab.size == SIZE_NORMAL) fab.size = SIZE_MINI
+            else fab.size = SIZE_NORMAL
+        }
     }
 }
